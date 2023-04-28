d71a368cfbba36cc5e8ce91c5be5b8a22a42fea6
Edward Güereque <edwardgnt@gmail.com>
2023-04-27 22:49:57 -0700

    arrays

---
+++ arrays/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$languages = ['PHP', 'Java', 'Python'];
+
+// push item to array
+
+// array_push($languages, 'C++');
+
+$languages[] = 'C++';
+
+
+// echo count($languages);
+
+// echo '<pre>';
+// print_r($languages);
+// echo '</pre>';
+
+
+// Associate Arrays
+
+$data = [
+    'php' => '8.0',
+    'python' => '3.9'
+];
+
+// Add a value to array
+$data['go'] = '1.15';
+
+
+echo '<pre>';
+print_r($data);
+echo '</pre>';
+
+
+
+
